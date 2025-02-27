2140f451ec121652b20b5c85c16fb953b0d97b7a
Govind Kumar <govind.kumar@IN-MB-50220.local>
2025-02-27 14:55:49 +0530

    terra-infra

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terra-bucket-dummy-gov25feb"
+  bucket = "govtest-cleo-test"
 }
 
 
--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "govtest-cleo"
+  bucket = "govtest-cleo-test"
 
   lifecycle {
     prevent_destroy = true

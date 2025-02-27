21a5797e4fdf4531e456e8d840acc4bd23e0ce3b
Govind Kumar <govind.kumar@IN-MB-50220.local>
2025-02-27 15:32:02 +0530

    terra-infra

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket         = "govtest-cleo-test"
+    bucket         = "govtest-cleo-test1"
     key            = "terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true

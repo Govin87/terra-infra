d9b16c65be25e9675d4c647c193c0baa8a168973
Govind Kumar <govind.kumar@IN-MB-50220.local>
2025-02-27 18:26:26 +0530

    terra-infra

--- s3.tf
+++ s3.tf
@@ -1,23 +1,23 @@
-resource "aws_s3_bucket" "terraform_state" {
+# Check if the S3 bucket already exists
+data "aws_s3_bucket" "terraform_state" {
   bucket = "govtest-cleo-test1"
+}
 
+# Create the bucket only if it doesn't exist
+resource "aws_s3_bucket" "terraform_state" {
+  count  = length(data.aws_s3_bucket.terraform_state.id) > 0 ? 0 : 1
+  bucket = "govtest-cleo-test1"
+  
   lifecycle {
     prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
   }
 
   tags = {
     Name = "Terraform State Bucket"
   }
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.terraform_state.id
 }
 
